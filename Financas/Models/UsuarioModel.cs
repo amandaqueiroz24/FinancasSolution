7b8210fe1bfe6bcf5c02fb231adc1d333c147efc
Amanda Queiroz Nunes <amanda.queiroz-@hotmail.com>
2019-11-14 13:14:29 -0300

    Primeira Versão do Projeto

---
+++ Financas/Models/UsuarioModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Financas.Models
+{
+	public class UsuarioModel
+	{
+		[Required]
+		public String Nome { get; set; }
+		[Required, EmailAddress]
+		public String Email { get; set; }
+		[Required]
+		public String  Senha { get; set; }
+		[Compare("Senha")]
+		public String ConfirmaSenha { get; set; }
+	}
+}
